6c79c4c21ec736af499b2594b1b12ca627ad6901
Melissa Christman <melchrstmn@gmail.com>
2019-09-04 11:45:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,17 @@
 def square_array(array)
-  
-  while 
+  counter = 0
+  results = []
+
+  while array[counter]
+    results[counter] = array[counter]**2
+    counter += 1
+  end
+
+  return results
 end
+
+numbers = [1,2,3]
+new_numbers = [9,10,16,25]
+
+square_array(numbers)
+square_array(new_numbers)
